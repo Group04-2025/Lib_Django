e9b86d18056eadd9efaa36a25eb6995438e78121
Rudik Avetisyan <rudikavetisyan6@gmail.com>
2025-11-29 14:46:37 +0400

    add forms

--- main_views/urls.py
+++ main_views/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.show_main, name = "main_page")
+    path("", views.show_main, name = "main_page"),
+    path("genre/<str:genre>", views.show_genres, name = "genre_page" ),
+    path("books/<str:title>", views.show_reviews, name = "review_page"),
 ]
 
 
--- main_views/views.py
+++ main_views/views.py
@@ -3,9 +3,23 @@ from .models import Book, Review
 
 
 def show_main(request):
+    books = Book.objects.all()
+    genre_set = set()
+    for book in books:
+        genre_set.add(book.genre)
+        
     return render(request, "index.html", {"books": Book.objects.all(),
-                                          "reviews": Review.objects.all()})
+                                          "reviews": Review.objects.all(),
+                                          "genres": genre_set })
 
 
+def show_genres(request, genre):
+    books = Book.objects.filter(genre = genre)
+    return render(request, "genre_page.html", {"books": books})
 
+
+def show_reviews(request, title):
+    book = Book.objects.get(title = title)
+    reviews = Review.objects.filter(book = book)
+    return render(request, "review_page.html", {"reviews": reviews})
 
